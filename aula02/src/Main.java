f553c247d9822c5b8fc549b6d206652c2593b278
VICTOR SCHMITZ ALVIM DE OLIVEIRA <victor_sa_oliveira@estudante.sesisenai.org.br>
2024-04-18 17:27:37 -0300

    Aula 4

---
+++ aula02/src/Main.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        Scanner teclado = new Scanner(System.in);
+//        System.out.print("Nome: ");
+//        String nome = teclado.nextLine();
+//
+//        System.out.print("Nota 1: ");
+//        float nota1 = teclado.nextFloat();
+//
+//        System.out.print("Nota 2 ");
+//        float nota2 = teclado.nextFloat();
+//
+//        System.out.print("Nota 3: ");
+//        float nota3 = teclado.nextFloat();
+//
+//         float media = ((nota1 + nota2 + nota3) / 3);
+//
+//        System.out.printf("A media do aluno %s foi %.2f", nome, media);
+//        if (media >= 7){
+//            System.out.println("Passou");
+//        }else{
+//            System.out.println("seu lixo");
+//        }
+
+//        JOptionPane.showMessageDialog(null,"ai cachorro");
+//        int idade = Integer.parseInt(JOptionPane.showInputDialog("Digite sua idade:"));
+//        System.out.println(idade);
+
+
+    }
+}
